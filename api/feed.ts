88230a21c3c7dfd9d52a2f62ddf5d42f93f60664
Shadow <jujutsukaigojosaturo@gmail.com>
2025-06-20 09:04:10 +0530

    Delete api directory

--- api/feed.ts
+++
@@ -1,8 +0,0 @@
-export default async function handler(request: Request): Promise<Response> {
-  const url = new URL(request.url);
-  const query = url.searchParams.get("q") ?? "none";
-
-  return new Response(`Feed page with query: ${query}`, {
-    headers: { "Content-Type": "text/plain" },
-  });
-}
