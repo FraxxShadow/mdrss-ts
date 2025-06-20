043426668b540528c755fe19738fa11eb7d84546
Shadow <jujutsukaigojosaturo@gmail.com>
2025-06-20 08:37:24 +0530

    Update feed.ts

--- backend/src/routes/feed.ts
+++ backend/src/routes/feed.ts
@@ -67,7 +67,7 @@ async function executeQueries(queries: ParsedQuery[]) {
 		.innerJoin(tUsers, eq(tChapters.uploaderUuid, tUsers.id))
 		.where(or(...queries.map(queryToSql)))
 		.orderBy(desc(tChapters.publishedAt))
-		.limit(200)
+		.limit(75)
 
 	const groupIds = Array.from(
 		new Set(results.flatMap(v => v.chapters.groupUuids ?? []))
